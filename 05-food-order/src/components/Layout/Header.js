80c5a2f186ff00296f1c4ac4c429982b027b701b
Dishant Arora <dishantarora16@gmail.com>
2023-03-01 12:23:57 +0530

    added header component

---
+++ 05-food-order/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import mealsImage from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles["header"]}>
+        <h1>Supr3meMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="A table of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;

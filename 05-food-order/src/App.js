8f27d353934b07571279e58c38c61ac0b3c32000
Dishant Arora <dishantarora16@gmail.com>
2023-03-06 15:33:17 +0530

    added cart reducer

--- 05-food-order/src/App.js
+++ 05-food-order/src/App.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [openCart, setOpenCart] = useState(false);
+
+  const showCartHandler = () => {
+    setOpenCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setOpenCart(false);
+  };
+
   return (
-    <>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {openCart && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 

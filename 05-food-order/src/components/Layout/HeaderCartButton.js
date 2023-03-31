261e885d1e6c2cb38daa10152a2b66d915877ecc
Dishant Arora <dishantarora16@gmail.com>
2023-04-01 00:21:08 +0530

    added bump animation on cart add

--- 05-food-order/src/components/Layout/HeaderCartButton.js
+++ 05-food-order/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+  const [itemAdded, setItemAdded] = useState(false);
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  let btnClasses = `${styles["button"]} ${itemAdded && styles["bump"]}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    setItemAdded(true);
+    const timer = setTimeout(() => {
+      setItemAdded(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles["button"]} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles["icon"]}>
         <CartIcon />
       </span>
